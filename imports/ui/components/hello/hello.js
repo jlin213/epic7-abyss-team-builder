c72be95e35a409f1cd91369632053fa5eb0f7a2f
Julianne Lin <julianne.lin@mail.utoronto.ca>
2019-06-14 21:49:35 -0400

    added full meteor files

--- imports/ui/components/hello/hello.js
+++ imports/ui/components/hello/hello.js
@@ -1,7 +1,4 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
-import './main.html';
+import './hello.html';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
