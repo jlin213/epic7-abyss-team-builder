1d60525c4bf64ee1a0d51d75b7fac8574d13bb91
WilsonYung <>
2019-06-15 20:07:09 -0700

    redirect

--- client/main.jsx
+++ client/main.jsx
@@ -1,8 +1,3 @@
-import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { render } from 'react-dom';
-import App from '/imports/ui/App'
+import '/imports/startup/client';
+import '/imports/startup/both';
 
-Meteor.startup(() => {
-  render(<App />, document.getElementById('react-target'));
-});
