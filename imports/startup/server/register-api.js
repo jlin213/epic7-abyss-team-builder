8a6b75bd84268988b05063f8f86f9f6be6822c22
WilsonYung <wilson@wyung.com>
2019-06-23 12:08:50 -0700

    register api

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -1,4 +1,7 @@
-//hearts db
+//heroes db
 import '../../api/heroes/methods.jsx';
 import '../../api/heroes/server/publications.jsx';
 
+//abyss db
+import '../../api/abyss/methods.jsx';
+import '../../api/abyss/server/publications.jsx';
