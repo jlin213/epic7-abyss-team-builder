26f15aef81df85c046ab6046b282071b9c965ac7
WilsonYung <wilson@wyung.com>
2019-06-23 13:27:10 -0700

    cleaned

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import { Meteor } from 'meteor/meteor';
-import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
-import { mount } from 'react-mounter';
-import { MainLayout } from '../../ui/layouts/mainlayout.jsx'
-import AccountsUIWrapper from '../../ui/components/AccountsUIWrapper.js'
-import FloorSelector from '../../ui/components/mainApp/FloorSelector.jsx';
-import FlexBoxes from '../../ui/components/mainApp/Flexboxes.jsx';
-import AddHero from "../../ui/components/admin/AddHero.jsx";
+import React 							from 'react';
+import { Meteor } 						from 'meteor/meteor';
+import { FlowRouter } 					from 'meteor/ostrio:flow-router-extra';
+import { mount } 						from 'react-mounter';
+// General
+import { MainLayout } 					from '../../ui/layouts/mainlayout.jsx';
+import AccountsUIWrapper 				from '../../ui/components/AccountsUIWrapper.js';
 
+// /admin
+import AdminButtons 					from '../../ui/components/admin/AdminButtons.jsx';
+import AddHero 							from "../../ui/components/admin/AddHero.jsx";
 FlowRouter.route("/admin", {
 	action: function(params){
 		mount(MainLayout, {
-			leftContent1: <AddHero />,
-			leftContent2: <FlexBoxes />,
+			leftContent1: <AdminButtons />,
 			loginbutton: <AccountsUIWrapper/>,
 		});
 	}
 })
+
+// /home
+import FloorSelector 					from '../../ui/components/mainApp/FloorSelector.jsx';
+import FlexBoxes 						from '../../ui/components/mainApp/Flexboxes.jsx';
 FlowRouter.route('*', {
 // do some action for this route
 action: function(params) {
--- imports/ui/components/admin/AddHero.jsx
+++ imports/ui/components/admin/AddHero.jsx
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 export default class AddHero extends Component{
 	constructor(props){
 		super(props);
-		this.state = {value: "", change: "", star: 0, elementType: ""}; 
+		this.state = {
+			value: "", 
+			change: "", 
+			star: 0, 
+			elementType: ""
+		}; 
+
 		this.handleSubmit = this.handleSubmit.bind(this);
     	this.handleChange = this.handleChange.bind(this);
     	this.handleSelectElement = this.handleSelectElement.bind(this);
@@ -14,61 +20,85 @@ export default class AddHero extends Component{
 	    this.setState({change: event.target.value});
 
 	}
-
 	handleSelectElement(event){
-		
 		this.setState({elementType: event.target.value});
 	}
 	handleSelectStar(event){
 		event.preventDefault(); 
 		this.setState({star: parseInt(event.target.value, 10)});
 	}
-
 	handleSubmit(event) {
 		event.preventDefault();
 	    this.setState({value: this.state.change}, function(){
 	    	Meteor.call('hero.add', this.state.value , this.state.star, this.state.elementType); 
 	    });
 	}
-
-	generateStars(){
-
-		const arr = [3, 4, 5]; 
-		const listStars = arr.map((num) => 
-			<option key={num +1} onSelect= {this.handleSelectStar}> {num}</option>
-		 )
-		return listStars;
-	}
-
-	generateElements(){  
-			const num = Array.from(Array(parseInt(5)).keys());
-			const arr = ["Fire", "Water", "Earth", "Light", "Dark"]; 
-			const listItems = num.map((num) => 
-				<option key={num +1} onSelect= {this.handleSelectElement}> {arr[num]}</option>
-			);
-			return listItems;
-	}
-
 	render(){
 		return(
-		<div>
-			<form onSubmit={this.handleSubmit}>
-			        <label>
-			        Enter the Hero Name:
-			       		<input type="text" value={this.state.change} onChange={this.handleChange} />
-			       	 Select the Star of the Hero: 
-			       	 <select className="custom-select" id="inputGroupSelect01" value= {this.state.star} onChange= {this.handleSelectStar}>
-				    	<option>Choose...</option>
-				    	{this.generateStars()}
-					</select>
-					Select the Element of the Hero:
-			       	<select className="custom-select" id="inputGroupSelect01" value= {this.state.elementType} onChange= {this.handleSelectElement}>
-				    	<option>Choose...</option>
-				    	{this.generateElements()}
-					</select>
-					<input type="submit" value="Submit"  />
-					</label>
-			</form>
+		<div className="">
+			<button type="button" 
+				className="btn btn-primary" 
+				data-toggle="modal" 
+				data-target="#addHeroes">
+				Add Heroes
+			</button>
+			<div id="addHeroes"	className="modal fade" role='dialog'>
+				<div className="modal-dialog modal-dialog-centered" role="document">
+					<div className="modal-content">
+						<div className="modal-header">
+							<h5 className="modal-title" id="exampleModalLabel">
+								Add heroe to available heroes list
+							</h5>
+							<button type="button" 
+								className="close" 
+								data-dismiss="modal" 
+								aria-label="Close">
+								<span aria-hidden="true">&times;</span>
+							</button>
+						</div>
+						<form onSubmit={this.handleSubmit}>
+							<div className="modal-body">
+								<div className="input-group mb-3">
+									<div className="input-group-prepend">
+										<label className="input-group-text" htmlFor="hero-name">Hero Name:</label>
+									</div>
+									<input id="hero-name"
+										className="form-control" 
+										type="text" 
+										value={this.state.change} 
+										onChange={this.handleChange} />
+								</div>
+								<div className="input-group mb-3 ">
+									<div className="input-group-prepend">
+										<label className="input-group-text" htmlFor="star-select">Natural Stars:</label>
+									</div>
+									<select className="custom-select" id="star-select">
+										<option disabled>Choose...</option>
+										<option value="3" onSelect={this.handleSelectStar}>3</option>
+										<option value="4" onSelect={this.handleSelectStar}>4</option>
+										<option value="5" onSelect={this.handleSelectStar}>5</option>
+									</select>
+
+									<div className="input-group-prepend ml-2">
+										<label className="input-group-text" htmlFor="star-select">Element:</label>
+									</div>
+									<select className="custom-select" id="star-select">
+										<option disabled>Choose...</option>
+										<option value="Fire" 	onSelect={this.handleSelectElement}>Fire</option>
+										<option value="Earth" 	onSelect={this.handleSelectElement}>Earth</option>
+										<option value="Water" 	onSelect={this.handleSelectElement}>Water</option>
+										<option value="Light" 	onSelect={this.handleSelectElement}>Light</option>
+										<option value="Dark" 	onSelect={this.handleSelectElement}>Dark</option>
+									</select>
+								</div>
+							</div>
+							<div className="modal-footer">
+								<button className="btn btn-primary" type="submit" value="Submit">Submit</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
 		</div>
 		)
 	}
