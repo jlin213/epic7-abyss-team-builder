6a21922716b051249ff7e634d75f6dfd57e965de
WilsonYung <wilson@wyung.com>
2019-06-22 19:11:32 -0700

    Added accounts

--- imports/api/heroes/methods.jsx
+++ imports/api/heroes/methods.jsx
@@ -5,14 +5,14 @@ import { heroDB } 								from './heroDB.jsx';
 
 
 Meteor.methods ({
-	'Hero.add'  ( name, natStar, element){
+	'Hero.add'( name, natStar, element){
 		if (!this.userId) {	
 			throw new Meteor.Error('insert not-authorized, Please log in first.');	
 		}
 		check(name, String);
 		check(natStar, Number);
 		check(element, String);
-		
+
 		heroDB.insert({
 			name: name,
 			natStar: natStar,
--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -1,8 +1,12 @@
 import React 							from 'react';
 import { Meteor } 						from 'meteor/meteor';
 import { FlowRouter } 					from 'meteor/ostrio:flow-router-extra';
-import {mount} 							from 'react-mounter';
-import {MainLayout} 					from '../../ui/layouts/mainlayout.jsx'
+import { mount } 						from 'react-mounter';
+import { MainLayout } 					from '../../ui/layouts/mainlayout.jsx'
+import AccountsUIWrapper				from '../../ui/components/AccountsUIWrapper.js'
+
+
+
 import FloorSelector 					from '../../ui/components/FloorSelector.jsx';
 import FlexBoxes 						from '../../ui/components/Flexboxes.jsx';
 
@@ -10,6 +14,7 @@ FlowRouter.route('*', {
 // do some action for this route
 action: function(params) {
 		mount(MainLayout, {
+			loginbutton: <AccountsUIWrapper />,
 		  	leftContent1: <FloorSelector />,
 		  	leftContent2: <FlexBoxes />,
 		});
