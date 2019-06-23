a235e6fb039919430bf78c0fd9014245f964c288
WilsonYung <wilson@wyung.com>
2019-06-22 18:16:58 -0700

    Heores insert added

--- imports/api/heroes/methods.jsx
+++ imports/api/heroes/methods.jsx
@@ -1,3 +1,24 @@
 import { check } 								from 'meteor/check';
 import { Meteor } 								from 'meteor/meteor';
-import { heroDB } 					from './heroDB.jsx';
+import { heroDB } 								from './heroDB.jsx';
+
+
+
+Meteor.methods ({
+	'Hero.add'  ( name, natStar, element){
+		if (!this.userId) {	
+			throw new Meteor.Error('insert not-authorized, Please log in first.');	
+		}
+		check(name, String);
+		check(natStar, Number);
+		check(element, String);
+		
+		heroDB.insert({
+			name: name,
+			natStar: natStar,
+			element: element,
+		});
+	},
+
+
+});
